14a5093e4d37b32e866925f5d88b3d8fc439bd01
Rachenko <alexey.rachenko@gmail.com>
2025-10-23 16:54:06 +0300

    Add project files.

---
+++ EduCloud-v42/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using EduCloud_v42.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduCloud_v42.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EduCloud-v42/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EduCloud_v42.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
