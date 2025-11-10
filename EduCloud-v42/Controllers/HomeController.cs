f6c21120298969a0686191764deb03347b6166c9
Timofiy Honcharov <tgjvua@gmail.com>
2025-11-10 04:30:21 +0200

    style changes

--- EduCloud-v42/Controllers/HomeController.cs
+++ EduCloud-v42/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace EduCloud_v42.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
